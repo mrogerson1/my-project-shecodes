ed511cd37364c9bd5b02540f5294e8596ee62a88
mrogerson1 <mrogerso@kent.edu>
2024-03-29 16:26:50 -0400

    Timezone Conversion
    Incorporated Google Map's Timezone API endpoint to dynamically adjust the time to be aligned with the local time in the city searched by the user

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ function refreshWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windSpeedElement = document.querySelector("#wind-speed");
-  let timeElement = document.querySelector("#time");
-  let date = new Date(response.data.time * 1000);
+  // let timeElement = document.querySelector("#time");
+  // old date
+  // let date = new Date(response.data.time * 1000);
   let iconElement = document.querySelector("#icon");
 
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
@@ -16,10 +17,11 @@ function refreshWeather(response) {
   temperatureElement.innerHTML = Math.round(temperature);
   windSpeedElement.innerHTML = `${response.data.wind.speed}m/h`;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-  timeElement.innerHTML = formatDate(date);
+  // timeElement.innerHTML = formatDate(date);
 
   getForecast(response.data.city);
 }
+/*
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
@@ -39,12 +41,73 @@ function formatDate(date) {
   }
 
   return `${day}${hours}:${minutes}`;
+}*/
+
+function getTimezone(response) {
+  // Extract latitude and longitude from the response
+  let latitude = response.data.coordinates.latitude;
+  let longitude = response.data.coordinates.longitude;
+  let timestamp = Math.floor(Date.now() / 1000);
+  let apiKey = "AIzaSyCstevPgCP0uRGxUJNhxN8YWFu74Id1s4U";
+  let apiUrl = `https://maps.googleapis.com/maps/api/timezone/json?location=${latitude}%2C${longitude}&timestamp=${timestamp}&key=${apiKey}`;
+  axios.get(apiUrl).then(convertDatetime);
+
+  console.log(timestamp);
+  console.log(Date(timestamp));
+}
+
+function convertDatetime(response) {
+  let localOffset = response.data.rawOffset * 1000;
+  let dstAdjust = response.data.dstOffset * 1000;
+  let timeZone = response.data.timeZoneName;
+  let utcTimestamp = Date.now();
+  let check_Date = new Date(utcTimestamp);
+  console.log(timeZone);
+  console.log(localOffset);
+
+  // 14400000 this number represents the conversion from EST --> UTC
+  // During Daylight savings another hour of miliseconds would need to be added
+  // 14400000 + 60*60*1000
+  let date = new Date(utcTimestamp + localOffset + dstAdjust + 14400000);
+  let timeElement = document.querySelector("#time");
+  timeElement.innerHTML = formatDate(date);
+  let timezoneElement = document.querySelector("#timezone");
+  timezoneElement.innerHTML = response.data.timeZoneName;
+  console.log(date);
+  console.log(Date(check_Date));
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? "PM" : "AM"; // Determine AM or PM
+  hours = hours % 12; // Convert hours to 12-hour format
+  hours = hours ? hours : 12; // Handle midnight (0 hours)
+
+  // Ensure minutes are displayed with leading zero if less than 10
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  // Get the day of the week
+  let days = [
+    "Sunday ",
+    "Monday ",
+    "Tuesday ",
+    "Wednesday ",
+    "Thursday ",
+    "Friday ",
+    "Saturday ",
+  ];
+  let day = days[date.getDay()];
+
+  // Construct the formatted time string
+  return `${day}${hours}:${minutes} ${ampm}`;
 }
 
 function searchCity(city) {
   let apiKey = "88ce19e7ct3e14a9169c6ob09cfa1a38";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
   axios.get(apiUrl).then(refreshWeather);
+  axios.get(apiUrl).then(getTimezone);
 }
 
 function handleSearchSubmit(event) {
